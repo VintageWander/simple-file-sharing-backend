b04fff82d8f1cd2640d95f9f0d4a0e3e7754e651
Andrew Giang <nguyendtgiang@gmail.com>
2023-05-29 08:58:43 +0700

    Refactor

--- src/folder/controller/mod.rs
+++ src/folder/controller/mod.rs
@@ -2,19 +2,21 @@ use axum::Router;
 
 use crate::GlobalState;
 
+use self::get::public::get_public_folders;
+
 pub mod create;
 pub mod delete;
 pub mod get;
 pub mod update;
 
-use {create::create_folder, delete::delete_folder, get::get_folders, update::update_folder};
+use {create::create_folder, delete::delete_folder, update::update_folder};
 
 pub fn folder_routes() -> Router<GlobalState> {
     Router::new().nest(
         "/folder",
         Router::new()
-            // /folder?parentFolderId={folder id}
-            .merge(get_folders())
+            // /folder/public
+            .merge(get_public_folders())
             // /folder/create
             .merge(create_folder())
             .merge(update_folder())
