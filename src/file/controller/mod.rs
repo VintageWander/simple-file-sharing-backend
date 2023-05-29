385c5492140fb4a6a47f14c4f2df83209145dfa5
Andrew Giang <nguyendtgiang@gmail.com>
2023-05-29 23:27:05 +0700

    Mount new routes

--- src/file/controller/mod.rs
+++ src/file/controller/mod.rs
@@ -4,8 +4,14 @@ use axum::Router;
 
 use crate::GlobalState;
 
-use self::get::get_files;
+use self::get::{my::get_my_files, public::get_public_files, shared::get_shared_files};
 
 pub fn file_routes() -> Router<GlobalState> {
-    Router::new().nest("/file", Router::new().merge(get_files()))
+    Router::new().nest(
+        "/file",
+        Router::new()
+            .merge(get_public_files())
+            .merge(get_my_files())
+            .merge(get_shared_files()),
+    )
 }
