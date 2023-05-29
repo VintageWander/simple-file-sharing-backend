895ee124e732f31d3c61336687efbabe7bd8a218
Andrew Giang <nguyendtgiang@gmail.com>
2023-05-29 21:45:54 +0700

    Add create file request model

---
+++ src/file/request/create.rs
@@ -0,0 +1,30 @@
+use axum::{async_trait, body::Body, extract::FromRequest, http::Request};
+use axum_typed_multipart::{FieldData, TryFromMultipart, TypedMultipart};
+use bytes::Bytes;
+use validator::Validate;
+
+use crate::{error::Error, prisma::Visibility, validation::uuid::check_uuid, GlobalState};
+
+#[derive(TryFromMultipart, Validate)]
+pub struct CreateFileRequest {
+    #[validate(custom = "check_uuid")]
+    pub parent: Option<String>,
+
+    pub visibility: Option<Visibility>,
+
+    pub file: FieldData<Bytes>,
+}
+
+#[async_trait]
+impl FromRequest<GlobalState, Body> for CreateFileRequest {
+    type Rejection = Error;
+    async fn from_request(
+        req: Request<Body>,
+        state: &GlobalState,
+    ) -> Result<Self, Self::Rejection> {
+        let TypedMultipart(body) =
+            TypedMultipart::<CreateFileRequest>::from_request(req, state).await?;
+        body.validate()?;
+        Ok(body)
+    }
+}
