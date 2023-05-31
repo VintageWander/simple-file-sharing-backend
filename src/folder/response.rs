721d324657a7904ed2955bfe9a846317286bf83b
Andrew Giang <nguyendtgiang@gmail.com>
2023-05-31 17:46:46 +0700

    Added child folders response model

--- src/folder/response.rs
+++ src/folder/response.rs
@@ -1,4 +1,4 @@
-use crate::prisma::folder;
+use crate::prisma::folder::{self, WhereParam};
 
 folder::select!(folder_response {
     id
@@ -12,4 +12,18 @@ folder::select!(folder_response {
     tags
     created_at
     updated_at
+});
+
+folder::select!((filters: Vec<WhereParam>) => child_folders_response {
+    child_folders(filters): select {
+        id
+        owner: select {
+            id username email created_at updated_at
+        }
+        parent_folder_id
+        folder_name
+        visibility
+        created_at
+        updated_at
+    }
 });
