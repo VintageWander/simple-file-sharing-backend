95523cc77187afd0fd9a647b9def343109aeffa4
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-19 22:54:04 +0700

    Added file version module

---
+++ src/file_version/model.rs
@@ -0,0 +1,12 @@
+use crate::prisma::file_version;
+
+file_version::select!(file_version_select {
+    file: select {
+        id
+        extension
+    }
+    version_number
+});
+
+pub type FileVersion = file_version::Data;
+pub type FileVersionSelect = file_version_select::Data;
