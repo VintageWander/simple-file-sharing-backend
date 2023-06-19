53a548d58d8c5ee1b905ef1e2830ba835470d942
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-19 22:49:41 +0700

    Now update file creates a new version for current file

--- src/file/controller/update.rs
+++ src/file/controller/update.rs
@@ -4,6 +4,7 @@ use axum_typed_multipart::{FieldData, FieldMetadata};
 use crate::{
     extractors::param::ParamId,
     file::model::{select::FileSelect, update::UpdateFileRequest},
+    file_version::model::FileVersionSelect,
     prisma::{file, folder},
     user::model::{loggedin::LoggedInUser, select::UserSelect},
     validation::validation_message,
@@ -17,6 +18,7 @@ pub fn update_file() -> Router<GlobalState> {
             db,
             folder_service,
             file_service,
+            file_version_service,
             storage,
             ..
         }): State<GlobalState>,
@@ -34,7 +36,7 @@ pub fn update_file() -> Router<GlobalState> {
             filename: target_filename,
             ..
         } = file_service
-            .get_file_by_user_id(vec![file::id::equals(file_id.clone())], user_id.clone())
+            .get_file_by_user_id(vec![file::id::equals(file_id)], user_id.clone())
             .await?;
 
         let parent_folder_id = match parent {
@@ -65,9 +67,44 @@ pub fn update_file() -> Router<GlobalState> {
             .await?;
 
         if let Some(data) = data {
-            storage.create_file(&updated_file.id, data).await?;
-        }
+            let FileVersionSelect {
+                file: updated_file,
+                version_number,
+            } = file_version_service
+                .create_version_for_file(updated_file.id)
+                .await?;
 
+            /*
+                Move the current file from
+                hello.txt
+                to
+                hello/123.txt
+            */
+            storage
+                .move_file(
+                    &format!("{}.{}", updated_file.id, updated_file.extension.to_string()),
+                    &format!(
+                        "{}/{}.{}",
+                        updated_file.id,
+                        version_number,
+                        updated_file.extension.to_string()
+                    ),
+                )
+                .await?;
+
+            /*
+                Use the bytes data to create a new file
+                Replacing that previous file
+            */
+            storage
+                .create_file(
+                    &format!("{}.{}", updated_file.id, updated_file.extension.to_string()),
+                    data,
+                )
+                .await?;
+
+            return Ok(Web::ok("Updated file successfully", updated_file));
+        }
         Ok(Web::ok("Updated file successfully", updated_file))
     }
     Router::new().route("/update/:file_id", put(update_file_handler))
