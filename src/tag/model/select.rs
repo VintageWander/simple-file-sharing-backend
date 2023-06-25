79921f6e07c0d825893a1b6d534686f6733189e8
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-25 15:05:43 +0700

    Added model for tags

---
+++ src/tag/model/select.rs
@@ -0,0 +1,32 @@
+use crate::prisma::tag;
+
+tag::select!(tag_select {
+    id
+    tag_name
+    owner: select {
+        id username email created_at updated_at
+    }
+    files: select {
+        id
+        owner: select {
+            id username email created_at updated_at
+        }
+        filename
+        extension
+        visibility
+
+        created_at
+        updated_at
+    }
+    folders: select {
+        id
+        owner: select {
+            id username email created_at updated_at
+        }
+        folder_name
+        visibility
+
+        created_at
+        updated_at
+    }
+});
