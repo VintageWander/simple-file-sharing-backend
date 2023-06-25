9d2e28cbc1d7936be3bd1f113e934df4bcc8aef2
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-25 23:33:00 +0700

    Added check uuids in a set

--- src/validation/uuid.rs
+++ src/validation/uuid.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use validator::ValidationError;
 
 use super::check_with;
@@ -8,4 +10,15 @@ pub fn check_uuid(uuid: &str) -> Result<(), ValidationError> {
         r#"^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$"#,
         "The id provided is not an UUID",
     )
+}
+
+pub fn check_uuid_set(uuids: &HashSet<String>) -> Result<(), ValidationError> {
+    for uuid in uuids {
+        check_with(
+            uuid,
+            r#"^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$"#,
+            "The id provided is not an UUID",
+        )?;
+    }
+    Ok(())
 }
