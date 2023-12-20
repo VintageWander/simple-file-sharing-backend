3ee7e4607719084024e4db56ca4b6b6789cfaf4e
Vintage Wander <nguyendtgiang@gmail.com>
2023-12-21 01:16:32 +0700

    fix: update try from multipart API

--- src/impls.rs
+++ src/impls.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use axum::{async_trait, extract::multipart::Field};
 use axum_typed_multipart::{TryFromField, TypedMultipartError};
 
@@ -9,7 +10,10 @@ use crate::{
 
 #[async_trait]
 impl TryFromField for Visibility {
-    async fn try_from_field(field: Field<'_>) -> Result<Self, TypedMultipartError> {
+    async fn try_from_field(
+        field: Field<'_>,
+        _: Option<usize>,
+    ) -> Result<Self, TypedMultipartError> {
         let visibility = match field.text().await?.as_str() {
             "public" => Visibility::Public,
             "private" => Visibility::Private,
@@ -18,6 +22,7 @@ impl TryFromField for Visibility {
                 return Err(TypedMultipartError::WrongFieldType {
                     field_name: "visibility".into(),
                     wanted_type: "public, private, or shared".into(),
+                    source: anyhow!(""),
                 })
             }
         };
