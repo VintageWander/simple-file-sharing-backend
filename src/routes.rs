04dbcfec8af6c6dd0b7aeda18fdd6dc29f43f8f6
Vintage Wander <nguyendtgiang@gmail.com>
2023-12-23 16:56:32 +0700

    feat: added preflight request to satisfy CORS

--- src/routes.rs
+++ src/routes.rs
@@ -1,13 +1,21 @@
-use axum::Router;
+use axum::{extract::State, routing::options, Router};
 
 use crate::{
     auth::controller::auth_routes, file::controller::file_routes,
     folder::controller::folder_routes, tag::controller::tag_route, user::controller::user_routes,
-    GlobalState,
+    web::Web, GlobalState, WebResult,
 };
+
+fn preflight() -> Router<GlobalState> {
+    async fn preflight_handler(_: State<GlobalState>) -> WebResult {
+        Ok(Web::ok("Preflight request passed", ()))
+    }
+    Router::new().route("/", options(preflight_handler))
+}
 
 pub fn routes() -> Router<GlobalState> {
     Router::new()
+        .merge(preflight())
         .merge(user_routes())
         .merge(auth_routes())
         .merge(folder_routes())
